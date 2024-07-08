777e630374d63c5c9f63b1428e59be9ab1218b16
Grigorii K. Shartsev <me@shgk.me>
2024-07-08 15:38:23 +0200

    start: 01-basics

---
+++ 01-basics/script.js
@@ -0,0 +1,4 @@
+// Все стили Meetups
+import '@shgk/vue-course-ui/meetups/style.css'
+// Копия ответа сервера по запросу /api/meetups для авторизованного пользователя
+import meetupsJson from './api/meetups.json'
