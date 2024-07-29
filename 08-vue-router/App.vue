2e7e9bb6c4318323997468441bcfd021d8b601c2
Grigorii K. Shartsev <me@shgk.me>
2024-07-29 17:20:27 +0200

    start: 08-vue-router

---
+++ 08-vue-router/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import MeetupsHeader from './components/MeetupsHeader.vue'
+import MeetupsFooter from './components/MeetupsFooter.vue'
+import PageMeetups from './components/PageMeetups.vue'
+import PageUser from './components/PageUser.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <MeetupsHeader />
+    <main class="main">
+      <PageMeetups />
+    </main>
+    <MeetupsFooter />
+  </div>
+</template>
+
+<style>
+.wrapper {
+  background-color: var(--grey-light);
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.main {
+  flex: 1 0 auto;
+}
+</style>
