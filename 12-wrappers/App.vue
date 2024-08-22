3d195c5166a3cec24876e251cfe05fed5340b3f5
Grigorii K. Shartsev <me@shgk.me>
2024-08-22 18:14:59 +0200

    start: 12-wrappers

---
+++ 12-wrappers/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { UiContainer, UiButton, UiIcon } from '@shgk/vue-course-ui'
+import UiInput from './UiInput.vue'
+// import UiInputNumber from './UiInputNumber.vue'
+// import UiInputField from './UiInputField.vue'
+
+const value = ref('')
+const numberValue = ref(0)
+</script>
+
+<template>
+  <UiContainer>
+    <p>
+      <UiButton>Focus</UiButton>
+    </p>
+
+    <h2>UiInput</h2>
+    <p>
+      Value: "{{ value }}"
+    </p>
+    <UiInput />
+
+<!--    <h2>UiInputNumber</h2>-->
+<!--    <p>-->
+<!--      Value: {{ numberValue }}-->
+<!--    </p>-->
+<!--    <UiInputNumber />-->
+
+<!--    <h2>UiInputField</h2>-->
+<!--    <p>-->
+<!--      Value: {{ value }}-->
+<!--    </p>-->
+<!--    <UiInputField />-->
+
+  </UiContainer>
+</template>
+
+<style scoped>
+h2 {
+  margin: 2rem 0 1rem 0;
+}
+
+p {
+  margin: 1rem 0;
+}
+</style>
---
+++ 12-wrappers/UiInputField.vue
@@ -0,0 +1,38 @@
+<script setup>
+import UiInput from './UiInput.vue'
+</script>
+
+<template>
+  <div class="form-group">
+    <label>
+      <span class="form-group__label"></span>
+      <UiInput />
+    </label>
+  </div>
+</template>
+
+<style scoped>
+/* _form-group.css */
+.form-group {
+  position: relative;
+  margin-bottom: 24px;
+}
+
+.form-group.form-group--inline {
+  display: inline-block;
+  margin-bottom: 0;
+}
+
+.form-group.form-group--inline + .form-group.form-group--inline {
+  margin-left: 16px;
+}
+
+.form-group__label {
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+  color: var(--body-color);
+  margin-bottom: 10px;
+  display: block;
+}
+</style>
