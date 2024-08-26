a5b1f45fee895dee62bfc172aea7f9e8be044623
Grigorii K. Shartsev <me@shgk.me>
2024-08-26 18:26:34 +0200

    13-reactivity

--- 13-reactivity/main.js
+++ 13-reactivity/main.js
@@ -63,28 +63,73 @@ function useDebouncedRef(source, delay = 200) {
   return debounced
 }
 
-// const a = ref(0)
-// const b = ref(0)
-//
-// const sum = computed(() => {
-//   console.log('[computed] sum')
-//   return a.value + b.value
-// })
-//
-// watch(a, () => console.log('[watch (pre)] a'), { flush: 'pre' })
-// watch(a, () => console.log('[watch (sync)] a'), { flush: 'sync' })
-// watch(a, () => console.log('[watch (post)] a'), { flush: 'post' })
-// watch(b, () => console.log('[watch (pre)] b'))
-// watch(sum, () => {
-//   console.log('[watch (pre)] sum')
-// })
-//
-// setTimeout(() => {
-//   Promise.resolve().then(() => {
-//     console.log('Resolved Promise')
-//   })
-//   console.log('Start updating A and B')
-//   a.value += 1
-//   b.value += 1
-//   console.log('Finish updating A and B')
-// })
+
+function createdDebouncedRef(value, delay = 200) {
+  let timeout
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track()
+        return value
+      },
+      set(newValue) {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          value = newValue
+          trigger()
+        }, delay)
+      },
+    }
+  })
+}
+
+function createSharedComposable(composable) {
+  let subscribers = 0
+  let state, scope
+
+  const dispose = () => {
+    if (scope && --subscribers <= 0) {
+      scope.stop()
+      state = scope = null
+    }
+  }
+
+  return (...args) => {
+    subscribers++
+    if (!state) {
+      scope = effectScope(true)
+      state = scope.run(() => composable(...args))
+    }
+    onScopeDispose(dispose)
+    return state
+  }
+}
+
+/***************
+ * Планировщик *
+ ***************/
+
+const a = ref(0)
+const b = ref(0)
+
+const sum = computed(() => {
+  console.log('[computed] sum')
+  return a.value + b.value
+})
+watch(a, () => console.log('[watch (pre)] a'), { flush: 'pre' })
+watch(a, () => console.log('[watch (sync)] a'), { flush: 'sync' })
+watch(a, () => console.log('[watch (post)] a'), { flush: 'post' })
+watch(b, () => console.log('[watch (pre)] b'))
+watch(sum, () => {
+  console.log('[watch (pre)] sum')
+})
+
+setTimeout(() => {
+  Promise.resolve().then(() => {
+    console.log('Resolved Promise')
+  })
+  console.log('Start updating A and B')
+  a.value += 1
+  b.value += 1
+  console.log('Finish updating A and B')
+})
