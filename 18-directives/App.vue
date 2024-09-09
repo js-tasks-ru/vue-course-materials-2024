379b06850343a93f84dbe16ac4835c213cab1525
Grigorii K. Shartsev <me@shgk.me>
2024-09-09 18:12:07 +0200

    start: 18-directives

---
+++ 18-directives/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('Input Value')
+
+function handleFocus(event) {
+  event.target.setSelectionRange(0, -1)
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <main class="main">
+      <input v-model="text" @focus="handleFocus" />
+    </main>
+  </div>
+</template>
+
+<style>
+.wrapper {
+  background-color: var(--grey-light);
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.main {
+  flex: 1 0 auto;
+}
+</style>
